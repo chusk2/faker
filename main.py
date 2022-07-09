7bbc353d7a323a5dff581af95fe33b0464952292
daniel <294.daniel@gmail.com>
2022-07-09 18:32:25 +0200

    Redifine code and new form of json file

--- main.py
+++ main.py
@@ -6,29 +6,18 @@ import json
 from faker import Faker
 
 fake = Faker()
-dic = {}
-for _ in range(10):
-    name = fake.prefix() + fake.name()
-    address = fake.address()
-    position = fake.job()
-    company = fake.company()
-    phone = fake.phone_number()
+people = []
+for i in range(100):
+    dic = {
+           'suffix': fake.suffix(),
+           'name': fake.name(),
+           'address': fake.address(),
+           'country': fake.country(),
+           'job': fake.job(),
+           'company': fake.company(),
+           'email': fake.email(),
+          }
+    people.append(dic)
 
-    dic.update({f'Person {_+1}':
-                {'name': name,
-                 'address': address,
-                 'phone': phone,
-                 'position': position,
-                 'company': company,
-                 }
-                })
-
-with open('profiles.json', 'w', encoding='utf-8') as f:
-    json.dump(dic, f, indent=4)
-
-for k, v in dic.items():
-    print(f'{k}: {v}')
-    print('\n')
-
-for i in fake.profile().keys():
-    print(i)
+with open('people.json', 'w', encoding='utf8') as file:
+    json.dump(people, file, indent=4)
